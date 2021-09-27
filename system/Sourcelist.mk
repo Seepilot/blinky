46315f16b652a3d54a8fb236b8d7774e8f08a599
Andreas Ritter <herr-a-ritter@web.de>
2021-09-27 22:52:16 +0200

    Created a new Sourcelist.mk for the system folder

--- system/Sourcelist.mk
+++ system/Sourcelist.mk
@@ -1,18 +1,13 @@
 ###################################################################################################
-######### Source list
-###################################################################################################
-
--include src/core/Sourcelist.mk
--include src/Sourcelist.mk
-
-###################################################################################################
-######### STM32 HAL sourcefiles
+######### STM32 HAL Source Files
 ###################################################################################################
 
 SRCS += system/stm32h7xx_hal_driver/Src/stm32h7xx_hal.c \
 	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_cortex.c \
 	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_gpio.c \
+	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_pwr.c \
 	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_pwr_ex.c \
 	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_rcc.c \
-	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_rtc_ex.c \
-	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_rtc.c
+	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_rcc_ex.c \
+	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_uart.c \
+	system/stm32h7xx_hal_driver/Src/stm32h7xx_hal_uart_ex.c
